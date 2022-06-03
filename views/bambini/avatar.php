c396b7590e381e85983f988aad16ae92a8e15e13
M Murdaca <m.murdaca1@studenti.uniba.it>
2022-06-03 16:33:24 +0200

    due pull
    1 avatar : solo ordine codice
    2: creazione grafico a torta

--- views/bambini/avatar.php
+++ views/bambini/avatar.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
-$this->title = "titolo";
+$this->title = "Avatar Personale";
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 
@@ -11,7 +11,12 @@ $this->params['breadcrumbs'][] = $this->title;
 $var = "https://sketchfab.com/models/1a7ac5e5268f4fbd88dacc9601c2ca4d/embed"; 
 ?>
 
-<iframe id="frame" style="border:0px;width:100%;height:500px" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+<iframe id="frame" style="border:0px;
+                   width:100%;
+                   height:500px"
+                allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true">
+
+</iframe>
 <script>
     let frame = document.getElementById("frame");
     let $var = <?php echo json_encode($var, JSON_HEX_TAG); ?>; // Don't forget the extra semicolon!
--- views/bambini/statistiche.php
+++ views/bambini/statistiche.php
@@ -24,15 +24,122 @@ if($media < 34){
     $testo = "Il Bambino sta andando molto bene!";
 }
 ?>
+
+
+<h1><?= Html::encode($this->title) ?></h1>
+<h1></h1>
+
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <script src="https://d3js.org/d3.v4.js"></script>
+    <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+</head>
+<body>
+<!-- Crea il div dove il grafico verrà posto -->
+<div id="grafico_donut"></div>
+
+
+<script>
+    // imposta dimensioni e margini del grafico
+    var width = 450
+    height = 450
+    margin = 100
+
+
+    var radius = Math.min(width, height) / 2 - margin
+
+    var svg = d3.select("#grafico_donut")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+    // Inseriamo i dati
+    var data = {Superati : <?= Html::encode($fattibene) ?> ,
+                Falliti : <?= Html::encode($fattimale) ?>
+    }
+
+
+    // imposta la scala dei color
+    var color = d3.scaleOrdinal()
+        .domain(["Superati", "Falliti"])
+        .range(d3.schemeDark2);
+
+
+    // Calcola la posizione di ogni gruppo sul grafico a torta:
+    var pie = d3.pie()
+        .sort(null) // Non ordinare il gruppo per dimensione
+        .value(function(d) {return d.value; })
+    var data_ready = pie(d3.entries(data))
+    // Generatore di arco
+    var arc = d3.arc()
+        .innerRadius(radius * 0.5) // Dimensioni del foro donut
+        .outerRadius(radius * 0.8)
+    // Quest'altro arco non verrà disegnato.
+    var outerArc = d3.arc()
+        .innerRadius(radius * 0.9)
+        .outerRadius(radius * 0.9)
+    // Costruisci il grafico a torta:
+    svg
+        .selectAll('allSlices')
+        .data(data_ready)
+        .enter()
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', function(d){ return(color(d.data.key)) })
+        .attr("stroke", "white")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+    // Aggiungi le polilinee tra il grafico e le etichette:
+    svg
+        .selectAll('allPolylines')
+        .data(data_ready)
+        .enter()
+        .append('polyline')
+        .attr("stroke", "black")
+        .style("fill", "none")
+        .attr("stroke-width", 1)
+        .attr('points', function(d) {
+            var posA = arc.centroid(d) // inserimento della linea nella fetta
+            var posB = outerArc.centroid(d) // interruzione di riga
+            var posC = outerArc.centroid(d); // Posizione etichetta = posB
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // angolo per la posizione X a destra o a sinistra
+            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // moltiplicare per 1 o -1 per metterlo a destra o a sinistra
+            return [posA, posB, posC]
+        })
+    // Aggiungi le polilinee tra il grafico e le etichette:
+    svg
+        .selectAll('allLabels')
+        .data(data_ready)
+        .enter()
+        .append('text')
+        .text( function(d) { console.log(d.data.key) ; return d.data.key } )
+        .attr('transform', function(d) {
+            var pos = outerArc.centroid(d);
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+            return 'translate(' + pos + ')';
+        })
+        .style('text-anchor', function(d) {
+            var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+            return (midangle < Math.PI ? 'start' : 'end')
+        })
+</script>
+</body>
+</html>
+
+
+
 <div class="bambini-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <h1><br><br></h1>
 
+    <h3>Il numero di esercizi fatti bene è: <?= Html::encode($fattibene) ?></h3>
 
-    <h2>Il numero di esercizi fatti bene è: <?= Html::encode($fattibene) ?></h2>
-
-    <h2>Il numero di esercizi fatti male è: <?= Html::encode($fattimale) ?></h2>
+    <h3>Il numero di esercizi fatti male è: <?= Html::encode($fattimale) ?></h3>
 
     <h2>La percentuale di successo degli esercizi è del: <?= Html::encode($media) ?> %</h2>
 
@@ -41,3 +148,5 @@ if($media < 34){
 
 
 </div>
+
+
